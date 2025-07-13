32bc24ab93be0f6908a9a0b639bce153f307df1b
CallMeJuu <justin.kobiolka@outlook.de>
2025-07-13 18:46:54 +0200

    Initial Commit

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.title = 'Love2D lets see where it goes'
+    t.window.width = 1920
+    t.window.height = 1080
+    t.window.resizable = false
+end
---
+++ main.lua
@@ -0,0 +1,21 @@
+
+local camera = require('lib.hump.camera')
+local class = require('lib.hump.class')
+local gamestate = require('lib.hump.gamestate')
+local timer = require('lib.hump.timer')
+local vector = require('lib.hump.vector')
+local anim8 = require('lib.anim8')
+local sti = require('lib.sti')
+
+function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+
+end
