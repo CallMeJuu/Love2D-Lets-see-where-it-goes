6dc4636215f8bbd8abf97f41a6f1e989100e7b49
CallMeJuu <justin.kobiolka@outlook.de>
2025-07-13 19:39:14 +0200

    new Screen Resolution for Retro look and Asset Naming Conventions

--- main.lua
+++ main.lua
@@ -6,16 +6,48 @@ local timer = require('lib.hump.timer')
 local vector = require('lib.hump.vector')
 local anim8 = require('lib.anim8')
 local sti = require('lib.sti')
+local screen = require("lib.sunscreen")
+
+local player = require('src.player')
 
 function love.load()
+    -- initiating a retro screen 
+    screen:init({
+        gameWidth = 480,
+        gameHeight = 270,
+        mode = "fit", -- Letterbox, pixel-perfect
+        debug = false
+    })
+    -- Filter for Pixel Art
     love.graphics.setDefaultFilter("nearest", "nearest")
+
+    player:init(200, 150)
+    player:load()
 
 end
 
 function love.update(dt)
-    
+    player:update(dt)
 end
 
 function love.draw()
+    -- Applying the Initiated Screen
+    screen:apply()
+    player:draw()
+end
 
+-- Love.rezise mousepressed and mousemoved still need to look into 
+function love.resize(w, h)
+    screen:onResize(w, h)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    local gameX, gameY = screen:screenToGame(x, y)
+    -- Use gameX, gameY for your game logic
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+    local gameX, gameY = screen:screenToGame(x, y)
+    local gameDX, gameDY = screen:screenToGame(dx, dy)
+    -- Use gameX, gameY, gameDX, gameDY as needed
 end
--- src/player.lua
+++ src/player.lua
@@ -19,24 +19,33 @@ function player:init(x , y)
 end
 
 function player:load()
-    self.spriteSheet = love.graphics.newImage('graphics/Player/Player.png')
+    self.spriteSheet = love.graphics.newImage('graphics/player/player.png')
 
     self.grid = anim8.newGrid(32,32,self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
 
     self.animation = {}
 
     -- Idle
-    self.animation.Idle_Down = anim8.newAnimation('1-6', 1)
-    self.animation.Idle_Right = anim8.newAnimation('1-6', 2)
-    self.animation.Idle_Left = anim8.newAnimation('1-6', 3)
-    self.animation.Idle_Up = anim8.newAnimation('1-6', 4)
+    self.animation.Idle_Down = anim8.newAnimation(self.grid('1-6', 1), 0.1)
+    self.animation.Idle_Right = anim8.newAnimation(self.grid('1-6', 2), 0.1)
+    self.animation.Idle_Left = anim8.newAnimation(self.grid('1-6', 3), 0.1)
+    self.animation.Idle_Up = anim8.newAnimation(self.grid('1-6', 4), 0.1)
 
     -- Walking
-    self.animation.Walk_Down = anim8.newAnimation('1-6', 5)
-    self.animation.Walk_Right = anim8.newAnimation('1-6', 6)
-    self.animation.Walk_Left = anim8.newAnimation('1-6', 7)
-    self.animation.Walk_Up = anim8.newAnimation('1-6', 8)
+    self.animation.Walk_Down = anim8.newAnimation(self.grid('1-6', 5), 0.1)
+    self.animation.Walk_Right = anim8.newAnimation(self.grid('1-6', 6), 0.1)
+    self.animation.Walk_Left = anim8.newAnimation(self.grid('1-6', 7), 0.1)
+    self.animation.Walk_Up = anim8.newAnimation(self.grid('1-6', 8), 0.1)
 
+    self.anim = self.animation.Idle_Down
+end
+
+function player:update(dt)
+    self.anim:update(dt)
+end
+
+function player:draw()
+    self.anim:draw(self.spriteSheet, self.x, self.y)
 end
 
 return player
